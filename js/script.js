3a4bf3a75efbd9f4be2b330fedfcb73816eb5887
Airelle M <Airelle.code@gmail.com>
2025-04-15 19:34:14 -0600

    Added async function to fetch image API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+let imagelist = [];
+
+async function getImage() {
+    const res = await fetch('https://picsum.photos/v2/list?limit=100');
+    const images = await res.json();
+    console.log(images); 
+    imagelist = images;
+  }
+  
+  getImage(); 
+
